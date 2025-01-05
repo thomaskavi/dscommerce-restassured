d9dc629e470581a0fb144e04069fd636afd63ea3
thomaskavi <thomaskavi2000@gmail.com>
2025-01-05 00:47:52 -0300

    first commit

---
+++ src/test/java/com/thomaskavi/dscommerce/dscommerce_restassured/DscommerceRestassuredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomaskavi.dscommerce.dscommerce_restassured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceRestassuredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
